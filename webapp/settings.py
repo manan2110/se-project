15319ed5c0f24a85bca8cca6f0828c6683646027
manan2110 <manan.co.in@gmail.com>
2022-03-10 13:56:14 +0530

    changes added

--- webapp/settings.py
+++ webapp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,8 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api",
-    'admin_interface',
-    'colorfield',
 ]
 
 MIDDLEWARE = [
