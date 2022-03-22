dad120b429302ff426a0c938fdf07376d2cb459b
Himanshu Kaushik <kaushikhimanshu303@gmail.com>
2022-03-23 03:33:46 +0530

    Added Signup and Login page

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
+from email.mime import base
 from django.urls import path
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('base/',views.base,name='base'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login')
 ]
--- api/views.py
+++ api/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "api/home.html", context={})
+
+def base(request):
+    return render(request, "api/base.html", context={})
+
+def signup(request):
+    return render(request, "api/signup.html", context={})
+
+def login(request):
+    return render(request, "api/login.html", context={})
+
