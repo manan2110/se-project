c58d4002411c9d316a9f2a447e9968c9bcc8b90f
Mercury_80 <harsh.gaud18@gmail.com>
2022-03-26 00:29:59 +0530

    Removed admin url form api app

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("base/", views.base, name="base"),
     path("signup/", views.signup, name="signup"),
