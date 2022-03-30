84f4b1bd47af5e8c7dad67993176e92542d221f8
Himanshu Kaushik <kaushikhimanshu303@gmail.com>
2022-03-31 04:15:17 +0530

    filters updated

--- api/filters.py
+++ api/filters.py
@@ -2,7 +2,8 @@ from dataclasses import field
 from .models import Shop
 import django_filters
 
+django_filters.ChoiceFilter
 class ShopFilter(django_filters.FilterSet):
     class Meta:
         model=Shop
-        fields=["category","name","location"]
+        fields={"category":['icontains'],"name":['icontains'],"location":['icontains']}
