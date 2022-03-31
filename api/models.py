5fa539535418b17f727724fd9004d037bcb0ad08
manan2110 <manan.co.in@gmail.com>
2022-03-31 13:48:34 +0530

    minor changes in ui

--- api/models.py
+++ api/models.py
@@ -2,52 +2,50 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
-from .managers import UserManager
 from . import choices as user_constants
 
 
 class User(AbstractUser):
     user_type = models.PositiveSmallIntegerField(
-        choices=user_constants.USER_TYPE_CHOICES,null = True,blank = True
+        choices=user_constants.USER_TYPE_CHOICES, null=True, blank=True
     )
     phone = models.CharField(max_length=15, blank=True)
 
 
 class Shop(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=512,blank=True,null=True)
-    category = models.CharField(max_length=255,blank=True,null=True)
-    address = models.JSONField(blank=True,null=True)
-    contact = models.JSONField(blank=True,null=True)
+    description = models.CharField(max_length=512, blank=True, null=True)
+    category = models.CharField(max_length=255, blank=True, null=True)
+    address = models.JSONField(blank=True, null=True)
+    contact = models.JSONField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.URLField(max_length=2084,null=True,blank=True)
-    location = models.CharField(max_length=128,null=True,blank = True)
+    image = models.URLField(max_length=2084, null=True, blank=True)
+    location = models.CharField(max_length=128, null=True, blank=True)
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255,blank=True,null=True)
-    description = models.CharField(max_length=512,blank=True,null=True)
-    price = models.FloatField(blank=True,null=True)
-    image = models.URLField(max_length=2084,blank=True,null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=512, blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    image = models.URLField(max_length=2084, blank=True, null=True)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    category = models.CharField(max_length=255,blank=True,null=True)
+    category = models.CharField(max_length=255, blank=True, null=True)
 
 
 class Subscription(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    price = models.FloatField(blank=True,null=True)
-    time_period = models.JSONField(blank=True,null=True)
-    quantity_list = models.JSONField(blank=True,null=True)
-    quantity = models.IntegerField(blank=True,null=True)
-    number_of_weeks = models.IntegerField(blank=True,null=True)
+    price = models.FloatField(blank=True, null=True)
+    time_period = models.JSONField(blank=True, null=True)
+    quantity_list = models.JSONField(blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    number_of_weeks = models.IntegerField(blank=True, null=True)
 
 
 class Order(models.Model):
     timestamp = models.DateTimeField(auto_now=True)
-    address = models.JSONField(blank=True,null=True)
+    address = models.JSONField(blank=True, null=True)
     products = models.ManyToManyRel(to=Product, field=Product)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     subscription = models.ForeignKey(to=Subscription, on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth import logout, login
 from .choices import USER_TYPE_CHOICES
 from django.contrib import messages
 from .forms import *
-from datetime import datetime, timedelta
 from .filters import ShopFilter
+
 # Create your views here.
+
 
 def home(request):
     if request.user.is_authenticated:
@@ -23,88 +24,80 @@ def buyer_dashboard(request):
         user = request.user
         subscriptions = Subscription.objects.filter(user=user.id)
         shops = Shop.objects.all()
-        context = {
-            'subscriptions':subscriptions,
-            'user':user,
-            'shops':shops
-            
-        }
-        return render(request,"api/buyerDashboard.html",context)
+        context = {"subscriptions": subscriptions, "user": user, "shops": shops}
+        return render(request, "api/buyerDashboard.html", context)
+
 
 def add_subscription(request):
-    
-    context = {
-    }
-    return render(request,"api/addSubscription.html",context)   
+
+    context = {}
+    return render(request, "api/addSubscription.html", context)
 
 
-def get_shop_products(request,pk):
+def get_shop_products(request, pk):
     products = Product.objects.filter(shop=pk)
-    context = {
-        'products':products
-    }
-    return render(request,"api/allProducts.html",context)
+    context = {"products": products}
+    return render(request, "api/allProducts.html", context)
+
 
 def get_all_shops(request):
     shops = Shop.objects.all()
-    filter=ShopFilter(request.GET,queryset=shops)
-    shops=filter.qs
-    context = {
-        'shops':shops,"filter":filter
-    }
-    return render(request,"api/allShops.html",context)
+    filter = ShopFilter(request.GET, queryset=shops)
+    shops = filter.qs
+    context = {"shops": shops, "filter": filter}
+    return render(request, "api/allShops.html", context)
 
 
-def product_details(request,pk):
+def product_details(request, pk):
     form = CreateUserForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         for key, value in request.POST.items():
-            print(f'Key: {key}')
-            print(f'Value: {value}') 
-        number_of_weeks = request.POST.get('week_counter')
-        quantity = request.POST.get('quantity')
+            print(f"Key: {key}")
+            print(f"Value: {value}")
+        number_of_weeks = request.POST.get("week_counter")
+        quantity = request.POST.get("quantity")
         user = request.user
-        
+
         # form = CreateUserForm(request.POST)
         # if form.is_valid():
         #     subscription = form.save()
         #     messages.success(request, 'Subscription Added')
         #     return redirect('buyer_dashboard')
         # else:
-        #     print(form.errors)   
-                     
+        #     print(form.errors)
+
     product = Product.objects.get(id=pk)
-    context = {
-        'product':product,
-        'form':form
-    }  
-    return render(request,"api/products.html",context)
+    context = {"product": product, "form": form}
+    return render(request, "api/products.html", context)
+
 
 def base(request):
-    return render(request, "api/base.html", context={})
+    return render(request, "api/checkout.html", context={})
 
 
 def sellers(request):
-    return render(request,"api/sellers.html",context={})
+    return render(request, "api/sellers.html", context={})
+
 
 def signup(request):
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                user = form.save()
-                username = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + username)
-                return redirect('login')
-            else:
-                print(form.errors)            
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " + username)
+            return redirect("login")
+        else:
+            print(form.errors)
 
-        context = {'form':form}
-        return render(request, 'api/signup.html', context)
+    context = {"form": form}
+    return render(request, "api/signup.html", context)
+
 
 def logoutUser(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
 
 
 def view_login(request):
@@ -124,6 +117,7 @@ def view_login(request):
 
         context = {}
         return render(request, "api/login.html", context)
+
 
 def logoutUser(request):
     logout(request)
