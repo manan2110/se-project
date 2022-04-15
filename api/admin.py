e64ed808bb04203e48723e6384d695431c3b6ca6
Mercury_80 <harsh.gaud18@gmail.com>
2022-04-15 23:19:47 +0530

    Modified Admin

--- api/admin.py
+++ api/admin.py
@@ -2,15 +2,39 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class FilterProductAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        # For Django < 1.6, override queryset instead of get_queryset
+        qs = super(FilterProductAdmin, self).get_queryset(request)
+        user = request.user
+        if not user.is_superuser:
+            if Shop.objects.filter(user=user).exists():
+                shop = Shop.objects.get(user=user)
+                return qs.filter(shop=shop)
+            return qs.filter(shop=None)
+        return qs
+
+class FilterSubscriptionAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        # For Django < 1.6, override queryset instead of get_queryset
+        qs = super(FilterSubscriptionAdmin, self).get_queryset(request)
+        user = request.user
+        if not user.is_superuser:
+            if Shop.objects.filter(user=user).exists():
+                shop = Shop.objects.get(user=user)
+                return qs.filter(shop=shop)
+            return qs.filter(shop=None)
+        return qs
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description", "category", "contact", "location")
+    list_display = ("id", "name", "description", "category", "contact", "location","user")
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "timestamp", "address", "user")
 
-class SubscriptionAdmin(admin.ModelAdmin):
+class SubscriptionAdmin(FilterSubscriptionAdmin):
     list_display = ("id", "product", "shop", "user","price","time_period","quantity","number_of_weeks","has_ordered")
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(FilterProductAdmin):
     list_display = ("id", "name", "description", "price","shop","category")
 
 class CartAdmin(admin.ModelAdmin):
